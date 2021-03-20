ca59c71b5517c7230a36c3043d6915bb7b076ac4
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-03-20 10:45:15 +0530

    fetching token works

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { getToken } from "./utils/getToken";
 function App() {
+  const [token, setToken] = useState("");
   useEffect(() => {
-    const token = getToken();
+    const token = getToken().access_token;
     console.log(token);
   }, []);
   return (
